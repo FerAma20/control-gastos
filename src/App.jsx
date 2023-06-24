0358cc784da66c166e30ba3278ce10bdf6fa7ed2
FerAma20 <ferama22@gmail.com>
2023-06-24 17:23:42 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react"
+import Header from "./components/header"
+import Modal from "./components/Modal"
+import ListadoGastos from "./components/ListadoGastos"
+import {generarId} from './helpers/index'
+import IconoNuevoGasto from "./img/nuevo-gasto.svg"
+
+
+
+function App() {
+  const [presupuesto, setPresupuesto] = useState(0)
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
+  const [modal, setModal] = useState(false)
+  const [animarModal, setAnimarModal] = useState(false)
+  const [gastos, setGastos] = useState([])
+
+  const handleNuevoGasto = () =>  {
+    setModal(true)
+    setTimeout(() => {
+      setAnimarModal(true)
+    }, 500);
+  }
+
+  const guardarGasto = gasto => {
+    gasto.id= generarId()
+    setGastos([...gastos, gasto ])
+    setAnimarModal(false)
+    setTimeout(() => {
+        setModal(false)
+    }, 300)
+  }
+
+  return (
+    <>
+      <h1>
+        <Header
+          presupuesto={presupuesto}
+          setPresupuesto={setPresupuesto}
+          isValidPresupuesto={isValidPresupuesto}
+          setIsValidPresupuesto={setIsValidPresupuesto}
+        />
+      </h1>
+
+
+      {isValidPresupuesto && (
+        <>
+          <main>
+            <ListadoGastos
+              gastos={gastos}
+            />
+          </main>
+          <div className="nuevo-gasto">
+            <img src={IconoNuevoGasto} alt="icono nuevo gasto" onClick={handleNuevoGasto} />
+          </div>
+        </>
+      )}
+
+      {modal && <Modal 
+        setModal={setModal}
+        animarModal = {animarModal}
+        setAnimarModal = {setAnimarModal}
+        guardarGasto = {guardarGasto}
+      />}
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Gasto.jsx
@@ -0,0 +1,9 @@
+const Gasto = ({key, gasto}) => {
+  return (
+    <div className="gasto sombra" >
+        <p>Desde Gasto</p>
+    </div>
+  )
+}
+
+export default Gasto
